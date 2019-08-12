d3dc1cf536a2150bc84807d5ccb177256b858c5a
FAREAST\chjinche <chjinche@microsoft.com>
2019-08-12 14:41:08 +0800

    Add det-seg-matterport

---
+++ azureml-custom-module-examples/det-seg-matterport/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="detsegmatterport",
+    version="0.0.1",
+    description="Object Detection and Instance Segmentation by Facebook Research",
+    packages=find_packages(),
+    author="Jincheng Chen",
+    license="MIT",
+    include_package_data=True,
+)
