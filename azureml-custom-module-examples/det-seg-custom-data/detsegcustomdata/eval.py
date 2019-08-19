cfc751fa27485a4a766c71dfdd76e69e235b1a30
FAREAST\chjinche <chjinche@microsoft.com>
2019-08-19 13:34:16 +0800

    Add evaluate

--- azureml-custom-module-examples/det-seg-custom-data/detsegcustomdata/eval.py
+++ azureml-custom-module-examples/det-seg-custom-data/detsegcustomdata/eval.py
@@ -55,41 +55,38 @@ class MaskRCNNScore:
         df = pd.DataFrame(out_img_str_list, columns=['result'])
         return df
 
-    # def evaluate(self, dataset_score_folder, prediction_folder):
-    #     run = Run.get_context()
-    #     dataset_score_path = os.path.join(dataset_score_folder, "dataset.pkl")
-    #     dataset_score = pickle.load(open(dataset_score_path, 'rb'))
-    #     if not dataset_score.image_info[random.choice(dataset_score.image_ids)]["polygons"] is None:
-    #         # Metrics
-    #         APs = compute_batch_ap(self.model, self.config, dataset_score, dataset_score.image_ids)
-    #         with open(os.path.join(prediction_folder, 'metrics.txt'), 'w') as fout:
-    #             fout.write("Mean AP overa {} images: {:.4f}".format(len(APs), np.mean(APs)))
-    #     # Predictions
-    #     for image_id in dataset_score.image_ids:
-    #         image = dataset_score.load_image(image_id)
-    #         info = dataset_score.image_info[image_id]
-    #         # logger.info("image ID: {}.{} ({}) {}".format(
-    #         #     info["source"], info["id"], image_id, dataset_score.image_reference(image_id)))
-    #         logger.info("image ID: {}.{} ({})".format(info["source"], info["id"], image_id))
-    #         # Run object detection
-    #         results = self.model.detect([image], verbose=1)
-    #         # Display results
-    #         r = results[0]
-    #         pred_filename = 'pred_{}'.format(info["id"].split('.jpg')[0])
-    #         fig = display_instances(image, boxes=r['rois'], masks=r['masks'],
-    #                                 class_ids=r['class_ids'], class_names=self.class_names,
-    #                                 scores=r['scores'], title="Predictions")
-    #         run.log_image("prediction/" + pred_filename, plot=fig)
-    #         fig.savefig(os.path.join(prediction_folder, pred_filename))
+    def evaluate(self, dataset_score_folder, prediction_folder):
+        run = Run.get_context()
+        dataset_score_path = os.path.join(dataset_score_folder, "dataset.pkl")
+        dataset_score = pickle.load(open(dataset_score_path, 'rb'))
+        if not dataset_score.image_info[random.choice(dataset_score.image_ids)]["polygons"] is None:
+            # Metrics
+            APs = compute_batch_ap(self.model, self.config, dataset_score, dataset_score.image_ids)
+            with open(os.path.join(prediction_folder, 'metrics.txt'), 'w') as fout:
+                fout.write("Mean AP overa {} images: {:.4f}".format(len(APs), np.mean(APs)))
+        # Predictions
+        for image_id in dataset_score.image_ids:
+            image = dataset_score.load_image(image_id)
+            info = dataset_score.image_info[image_id]
+            # logger.info("image ID: {}.{} ({}) {}".format(
+            #     info["source"], info["id"], image_id, dataset_score.image_reference(image_id)))
+            logger.info("image ID: {}.{} ({})".format(info["source"], info["id"], image_id))
+            # Run object detection
+            results = self.model.detect([image], verbose=1)
+            # Display results
+            r = results[0]
+            pred_filename = 'pred_{}'.format(info["id"].split('.jpg')[0])
+            fig = display_instances(image, boxes=r['rois'], masks=r['masks'],
+                                    class_ids=r['class_ids'], class_names=self.class_names,
+                                    scores=r['scores'], title="Predictions")
+            run.log_image("prediction/" + pred_filename, plot=fig)
+            fig.savefig(os.path.join(prediction_folder, pred_filename))
 
 
-def test(model_folder, input_df_folder, dataset_score_folder, prediction_folder, model_filename):
+def test(model_folder, dataset_score_folder, prediction_folder, model_filename):
     meta = {'Model file': str(model_filename)}
     maskrcnn = MaskRCNNScore(model_folder, meta=meta)
-    input_df_path = os.path.join(input_df_folder, 'data.dataset.parquet')
-    if os.path.exists(input_df_path):
-        input_df = pd.read_parquet(input_df_path, engine='pyarrow')
-        maskrcnn.run(input_df=input_df)
+    maskrcnn.evaluate(dataset_score_folder, prediction_folder)
 
 
 if __name__ == '__main__':
