7c5675a653fe4b9ea0f1d45f0a19fea31f84a357
Ubuntu <aml-custom-module@aml-custom-module-vm2.naphbvfijznujpbmy5u3khmp5b.xx.internal.cloudapp.net>
2019-11-18 11:16:52 +0000

    Add schema for pred dataframe

--- azureml-custom-module-examples/densenet-img-cls-official/densenetimgclsoff/score.py
+++ azureml-custom-module-examples/densenet-img-cls-official/densenetimgclsoff/score.py
@@ -1,50 +1,55 @@
 import pandas as pd
 import pyarrow.parquet as pq  # noqa: F401 workaround for pyarrow loaded
-from PIL import Image
-from io import BytesIO
 from .utils import get_transform, logger, torch_loader
 # from utils import get_transform, logger, torch_loader
-import base64
 import os
 import fire
 import torch
 import torch.nn as nn
+from torchvision import transforms
 from azureml.studio.core.io.model_directory import load_model_from_directory
 from azureml.studio.core.io.data_frame_directory import save_data_frame_to_directory
 from azureml.studio.core.io.image_directory import ImageDirectory
+from azureml.studio.modules.ml.common.ml_utils import generate_score_column_meta, TaskType
+from azureml.studio.modules.ml.common.constants import ScoreColumnConstants
+from azureml.studio.common.datatable.data_table import DataTable
 
 
 class Score:
     def __init__(self, model_path, meta={}):
-        _, self.inference_transforms = get_transform()
+        self.to_tensor_transform = transforms.Compose([transforms.ToTensor()])
         self.model, self.id_to_class_dict = load_model_from_directory(
             model_path, model_loader=torch_loader).data
         self.model.eval()
 
     def run(self, loader_dir, meta=None):
         my_list = []
-        for img, _, identifier in loader_dir.iter_images():
-            # print(img)
-            try:
-                input_tensor = self.inference_transforms(img)
-                input_tensor = input_tensor.unsqueeze(0)
-                if torch.cuda.is_available():
-                    input_tensor = input_tensor.cuda()
-                with torch.no_grad():
-                    output = self.model(input_tensor)
-                    softmax = nn.Softmax(dim=1)
-                    pred_probs = softmax(output).cpu().numpy()[0]
-                    index = torch.argmax(output, 1)[0].cpu().item()
-                result = [
-                    self.id_to_class_dict[str(index)], pred_probs[index],
-                    identifier
-                ]
-                my_list.append(result)
-            except Exception:
-                logger.info(f'Exception {identifier}')
-                pass
-        df = pd.DataFrame(my_list,
-                          columns=['category', 'probability', 'identifier'])
+        for img, label, identifier in loader_dir.iter_images():
+            # print(f'label: {label}')
+            input_tensor = self.to_tensor_transform(img)
+            input_tensor = input_tensor.unsqueeze(0)
+            if torch.cuda.is_available():
+                input_tensor = input_tensor.cuda()
+            with torch.no_grad():
+                output = self.model(input_tensor)
+                softmax = nn.Softmax(dim=1)
+                pred_probs = softmax(output).cpu().numpy()[0]
+            index = torch.argmax(output, 1)[0].cpu().item()
+            result = [identifier, label, self.id_to_class_dict[str(index)]
+                      ] + list(pred_probs)
+            my_list.append(result)
+
+        schema_columns = [
+            'identifier', 'label', ScoreColumnConstants.ScoredLabelsColumnName
+        ] + [
+            f'{ScoreColumnConstants.ScoredProbabilitiesMulticlassColumnNamePattern}_{self.id_to_class_dict[str(i)]}'
+            for i in range(len(self.id_to_class_dict))
+        ]
+        logger.info(f'schema: {schema_columns}')
+        df = pd.DataFrame(my_list, columns=schema_columns)
+        if df['label'].isnull().any():
+            logger.info("Remove label because input data is not of 'ImageFolder' type")
+            df.drop(columns=['label'], inplace=True)
         return df
 
     def infer(self, data_path, save_path):
@@ -52,13 +57,17 @@ class Score:
         loader_dir = ImageDirectory.load(data_path)
         logger.info(f'Predicting:')
         pred_df = self.run(loader_dir)
-        save_data_frame_to_directory(save_path, data=pred_df)
+        score_columns = generate_score_column_meta(
+            task_type=TaskType.MultiClassification, predict_df=pred_df)
+        pred_dt = DataTable(pred_df)
+        pred_dt.meta_data.score_column_names = score_columns
+        save_data_frame_to_directory(save_path, data=pred_dt.data_frame, schema=pred_dt.meta_data.to_dict())
         logger.info("DataFrame dumped")
 
 
 def entrance(model_path='/mnt/chjinche/projects/saved_model',
-             data_path='/mnt/chjinche/data/small/',
-             save_path='/mnt/chjinche/data/scored'):
+             data_path='/mnt/chjinche/data/test_data/',
+             save_path='/mnt/chjinche/data/scored_nolabel'):
     score = Score(model_path)
     logger.info("model init finished.")
     score.infer(data_path=data_path, save_path=save_path)
