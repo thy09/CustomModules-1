a4edd5e033fc6061f469a9bfc7fef176d0cce935
Ubuntu <aml-custom-module@aml-custom-module-vm2.naphbvfijznujpbmy5u3khmp5b.xx.internal.cloudapp.net>
2019-12-26 09:12:24 +0000

    test compressed file

--- azureml-custom-module-examples/image-classification/imageclassification/convert_to_image_directory.py
+++ azureml-custom-module-examples/image-classification/imageclassification/convert_to_image_directory.py
@@ -1,16 +1,18 @@
 import fire
 from azureml.studio.core.logger import logger
-from azureml.studio.core.io.image_directory import FolderBasedImageDirectory
+from azureml.studio.core.io.image_directory import ImageDirectory
+# from azureml.studio.core.io.image_directory import FolderBasedImageDirectory
 
 
-def entrance(input_path='../image_dataset/',
-             output_path='../image_dir/'):
+def entrance(input_path='/mnt/chjinche/test_data/256_ObjectCategories.zip',
+             output_path='/mnt/chjinche/test_data/image_dir/'):
     logger.info('Start!')
     # Case 1: input path is torchvision ImageFolder
+    # loader_dir = FolderBasedImageDirectory.load_organized(input_path)
     # TODO: Case 2: input path is custom image format
-    loader_dir = FolderBasedImageDirectory.load_organized(input_path)
+    loader_dir = ImageDirectory.load_compressed(input_path)
     loader_dir.dump(output_path)
-    logger.info('Finished')
+    logger.info('Finished.')
 
 
 if __name__ == '__main__':
