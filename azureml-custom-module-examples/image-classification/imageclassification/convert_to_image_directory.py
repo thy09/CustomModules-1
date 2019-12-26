2436f4da9af9cb09dd101f90b7ccda1496466fa4
Ubuntu <aml-custom-module@aml-custom-module-vm2.naphbvfijznujpbmy5u3khmp5b.xx.internal.cloudapp.net>
2019-12-26 12:54:40 +0000

    get compressed file path

--- azureml-custom-module-examples/image-classification/imageclassification/convert_to_image_directory.py
+++ azureml-custom-module-examples/image-classification/imageclassification/convert_to_image_directory.py
@@ -1,16 +1,24 @@
 import fire
+from pathlib import Path
 from azureml.studio.core.logger import logger
 from azureml.studio.core.io.image_directory import ImageDirectory
 # from azureml.studio.core.io.image_directory import FolderBasedImageDirectory
 
 
-def entrance(input_path='/mnt/chjinche/test_data/256_ObjectCategories.zip',
+def entrance(input_path='/mnt/chjinche/test_data/',
              output_path='/mnt/chjinche/test_data/image_dir/'):
     logger.info('Start!')
     # Case 1: input path is torchvision ImageFolder
     # loader_dir = FolderBasedImageDirectory.load_organized(input_path)
     # TODO: Case 2: input path is custom image format
-    loader_dir = ImageDirectory.load_compressed(input_path)
+    compressed_extensions = {'.tar', '.zip'}
+    compressed_path = None
+    for path in Path(input_path).glob(r'**/*'):
+        if path.suffix in compressed_extensions:
+            compressed_path = path
+
+    logger.info(f'compressed file path {compressed_path}')
+    loader_dir = ImageDirectory.load_compressed(compressed_path)
     loader_dir.dump(output_path)
     logger.info('Finished.')
 
