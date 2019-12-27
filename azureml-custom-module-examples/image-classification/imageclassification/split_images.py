0fc0b659269e54343a8548c820d89b2e0e1aa710
Heyi Tang <heta@microsoft.com>
2019-12-27 15:45:33 +0800

    Update split_images

--- azureml-custom-module-examples/image-classification/imageclassification/split_images.py
+++ azureml-custom-module-examples/image-classification/imageclassification/split_images.py
@@ -5,20 +5,21 @@ from azureml.studio.core.io.image_directory import ImageDirectory, FolderBasedIm
 
 
 def get_stratified_split_list(lst, fraction):
-    labels = [d.get('class', '') for d in lst]
-    train_lst, test_lst, train_label, test_label = train_test_split(
-        lst, labels, stratify=labels, train_size=fraction)
-    return train_lst, test_lst
+    n = len(lst)
+    labels = [d['category_id'] for d in lst]
+    train_idx, test_idx, train_label, test_label = train_test_split(
+        list(range(n)), labels, stratify=labels, train_size=fraction)
+    return train_idx, test_idx
 
 
 def split_images(src_path, tgt_train_path, tgt_test_path, fraction):
     loaded_dir = ImageDirectory.load(src_path)
     lst = loaded_dir.image_lst
     logger.info(f'Start splitting.')
-    train_set_lst, test_set_lst = get_stratified_split_list(lst, fraction)
-    logger.info(f'Got stratified split list. train {len(train_set_lst)}, test {len(test_set_lst)}.')
-    train_set_dir = FolderBasedImageDirectory.create_with_lst(src_path, train_set_lst)
-    test_set_dir = FolderBasedImageDirectory.create_with_lst(src_path, test_set_lst)
+    train_set_idx, test_set_idx = get_stratified_split_list(lst, fraction)
+    logger.info(f'Got stratified split list. train {len(train_set_idx)}, test {len(test_set_idx)}.')
+    train_set_dir = loaded_dir.get_sub_dir(train_set_idx)
+    test_set_dir = loaded_dir.get_sub_dir(test_set_idx)
     logger.info('Dump train set.')
     train_set_dir.dump(tgt_train_path)
     logger.info('Dump test set.')
